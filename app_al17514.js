4783b9530639d310adcefd7ceeb29c4028d14354
Issac David Puga Vazquez <al17514@cbtis051.edu.mx>
2019-11-26 12:16:56 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17514.js
+++ app_al17514.js
@@ -3,5 +3,9 @@ http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type':'application/json; charset=utf-8'});
 
     res.end('Hola Mundo Node JS Repo GIT - Issac Puga Vazquez/n');
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
+
 }).listen(8081);
 console.log('Server running on port 8081');
