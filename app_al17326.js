279b524bb300e3443d612acb37e13d623f2df27f
Karin Enrique Torres Noriega <al17326@cbtis051.edu.mx>
2019-11-26 14:51:18 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17326.js
+++ app_al17326.js
@@ -1,7 +1,11 @@
 var http = require('http');
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+var objeto = {
+    mensaje : 'Hola mundo Node JS Repo Git - Torres Noriega Karin Enrique\n'
+};
+var json = JSON.stringify(objeto);
+res.end(json);
 
-res.end('Hola mundo Node JS REPO  GIT- Torres Noriega Karin Enrique\n');
 }).listen(8081);
 console.log('Server running on port 8081');
