5124ff099999a45cd45cdee1c4087cb34a55de7e
Naidelyn Tamayo Ruiz <al170310@cbtis051.edu.mx>
2019-11-26 19:31:58 -0700

    Ajustes en la respuesta a formato JSON

--- app_al170310.js
+++ app_al170310.js
@@ -1,8 +1,11 @@
 var http = require('http');
-http.createServer(function (req, res){
-    res.writeHead(200, {'Content-Type':'application/json; charset=utf-8 /n'});
-
-    res.end('Hola mundo Node JS Repo GIT - naidelyn Tamayo Ruiz');
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+var objeto = {
+    mensaje : 'Hola mundo Node JS Repo Git - Tamayo Ruiz Naidelyn\n'
+};
+var json = JSON.stringify(objeto);
+res.end(json);
 
 }).listen(8081);
 console.log('Server running on port 8081');
